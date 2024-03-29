842c4e1d452e43b1ff35282c21d37d477d58ae55
huangg <huangg@eetrust.com>
2024-03-29 09:51:53 +0800

    新疆电信数据同步初始化

--- src/main/java/com/eetrust/controller/aa.java
+++ src/main/java/com/eetrust/controller/aa.java
@@ -1,13 +1,9 @@
 package com.eetrust.controller;
 
-import com.eetrust.util.WebServiceUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static com.eetrust.util.WebServiceUtils.webserviceInvok;
 
 /**
  * @Author huangg
@@ -20,16 +16,9 @@ public class aa {
     @RequestMapping("/bb")
 
     public String bb(){
-        String xml="<root><privateKey>UAP_2oSY90</privateKey><srcContent></srcContent><dataContent><syncContent dataType=\"1\" operType=\"1\"><syncUnicode>0410000043</syncUnicode><newContent><baseInfo><deptName>顺丰</deptName><deptUniCode>0410000043</deptUniCode><showNum>9999</showNum><deptStatus>1</deptStatus><isCorp>1</isCorp></baseInfo><parentInfo><parentCode>0410000043</parentCode></parentInfo></newContent></syncContent></dataContent></root>";
+        String xml="测试接口-------------";
         log.info("xml->"+xml);
-       // webserviceInvok("http://10.3.44.33:8090/securedoc/clientinterface/syncData/ISyncWebService",xml);
         return "200";
     }
-    @RequestMapping("/cc")
-    public String cc(){
-        String xml="<root><privateKey>UAP_2oSY90</privateKey><srcContent /><dataContent><syncContent dataType=\"2\" operType=\"1\"><syncUnicode>zengwj</syncUnicode><newContent><baseInfo><loginName>zengwj</loginName><accountStatus>1</accountStatus><userName>曾文菁</userName><secLevel>2</secLevel></baseInfo><parentInfo><parentCode>0410000043</parentCode></parentInfo></newContent></syncContent></dataContent></root>";
-        log.info("xml->"+xml);
-      //  webserviceInvok("http://10.3.44.33:8090/securedoc/clientinterface/syncData/ISyncWebService",xml);
-        return "200";
-    }
+
 }
--- src/main/resources/readme.md
+++
@@ -1,4 +0,0 @@
-# 启动命令 
-> nohup java -jar sm-sync-sf-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
-
-
