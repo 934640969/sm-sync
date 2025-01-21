8d07c0deb76e9f3e38d4d786551f023ad6dcba8b
huangg <huangg@eetrust.com>
2025-01-21 09:26:49 +0800

    增加过滤，调整字段取值

--- src/main/java/com/eetrust/impl/UserSync3.java
+++ src/main/java/com/eetrust/impl/UserSync3.java
@@ -1,9 +1,12 @@
 package com.eetrust.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.sf.bdus.dist.client.repository.DataRepository;
 import com.sf.bdus.dist.common.context.DataContext;
-import com.sf.bdus.dist.common.dto.AggEmpDTO;
+import com.sf.bdus.dist.common.dto.UcUserAccountDataDTO;
 import com.sf.bdus.dist.common.dto.UcUserInfoDataDTO;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,7 +32,7 @@ public class UserSync3 implements DataRepository<UcUserInfoDataDTO> {
     private String smUrl;
 
     @Override
-    public void save(Iterator<UcUserInfoDataDTO> iterator, DataContext dataContext) throws IOException {
+    public void save(Iterator<UcUserInfoDataDTO> iterator, DataContext dataContext) {
         log.info("开始同步UcUserInfoDataDTO数据");
         final int batchSize = 100; // 每次保存的记录数目，在处理大量数据时，为了避免将全部数据加载到内存，从而导致内存溢出
         List<UcUserInfoDataDTO> ucUserInfoDataDTOS = new ArrayList<>(batchSize);
@@ -51,21 +54,45 @@ public class UserSync3 implements DataRepository<UcUserInfoDataDTO> {
     private void baocun(List<UcUserInfoDataDTO> ucUserInfoDataDTOS) {
         log.info("开始保存UcUserInfoDataDTO数据");
         for (UcUserInfoDataDTO ucUserInfoDataDTO:ucUserInfoDataDTOS){
+            String jobNumber = ucUserInfoDataDTO.getJobNumber();
+            String name = ucUserInfoDataDTO.getName();
+            List<UcUserAccountDataDTO> accounts = ucUserInfoDataDTO.getAccounts();
+            if (StringUtils.isEmpty(jobNumber)){
+                continue;
+            }
+            if (StringUtils.isEmpty(name)){
+                continue;
+            }
+            if (accounts==null){
+                continue;
+            }
+            String authority = accounts.get(0).getAuthority();
+            if (authority==null||"null".equals(authority)||"".equals(authority)){
+                continue;
+            }
+            String jsonString = JSON.toJSONString(ucUserInfoDataDTO);
+            log.info("jsonString->"+jsonString);
+            log.info("authority->"+authority);
+            JSONObject jsonObject = JSON.parseObject(authority);
+            String colDeptid = jsonObject.getString("colDeptid");
+            if (StringUtils.isEmpty(colDeptid)){
+                continue;
+            }
             String xml="<root>" +
                     "<privateKey>UAP_2oSY90</privateKey>" +
                     "<srcContent></srcContent>" +
                     "<dataContent>" +
                     "<syncContent dataType=\"2\" operType=\"1\">" +
-                    "<syncUnicode>"+ucUserInfoDataDTO.getJobNumber()+"</syncUnicode>" +
+                    "<syncUnicode>"+jobNumber+"</syncUnicode>" +
                     "<newContent>" +
                     "<baseInfo>" +
-                    "<loginName>"+ucUserInfoDataDTO.getJobNumber()+"</loginName>" +
+                    "<loginName>"+jobNumber+"</loginName>" +
                     "<accountStatus>1</accountStatus>" +
-                    "<userName>"+ucUserInfoDataDTO.getName()+"</userName>" +
+                    "<userName>"+name+"</userName>" +
                     "<secLevel>5</secLevel>" +
                     "</baseInfo>" +
                     "<parentInfo>" +
-                    "<parentCode>"+ucUserInfoDataDTO.getAccounts().get(0).getAuthority()+"</parentCode>" +
+                    "<parentCode>"+colDeptid+"</parentCode>" +
                     "</parentInfo>" +
                     "</newContent></syncContent></dataContent></root>";
             webserviceInvok(smUrl,xml);
