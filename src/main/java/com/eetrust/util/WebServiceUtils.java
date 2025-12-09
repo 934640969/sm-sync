65178d1c6703354ebd89d8967687ccd470cc499d
huangg <huangg@eetrust.com>
2025-12-09 14:28:46 +0800

    修复漏洞，升级axis2

--- src/main/java/com/eetrust/util/WebServiceUtils.java
+++ src/main/java/com/eetrust/util/WebServiceUtils.java
@@ -1,45 +1,71 @@
 package com.eetrust.util;
 
-import org.apache.axis.client.Call;
-import org.apache.axis.client.Service;
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.OMNamespace;
+import org.apache.axis2.addressing.EndpointReference;
+import org.apache.axis2.client.Options;
+import org.apache.axis2.client.ServiceClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.namespace.QName;
-import java.net.URL;
-
 public class WebServiceUtils {
-	private static final Logger log = LoggerFactory.getLogger(WebServiceUtils.class);
-	public static void main(String[] args) {
-		// webservice调用
-		String xml="<root><privateKey>UAP_2oSY90</privateKey><srcContent></srcContent><dataContent><syncContent dataType=\"1\" operType=\"1\"><syncUnicode>0410000043</syncUnicode><newContent><baseInfo><deptName>顺丰</deptName><deptUniCode>0410000043</deptUniCode><showNum>9999</showNum><deptStatus>1</deptStatus><isCorp>1</isCorp></baseInfo><parentInfo><parentCode>0410000043</parentCode></parentInfo></newContent></syncContent></dataContent></root>";
-		System.out.println(xml);
-		webserviceInvok("http://10.3.44.33:8090/securedoc/clientinterface/syncData/ISyncWebService",xml);
-	}
+    private static final Logger log = LoggerFactory.getLogger(WebServiceUtils.class);
 
-	public static String webserviceInvok(String endpoint, String xml) {
+
+    /**
+     * 使用Axis2调用WebService接口
+     * @param endpoint WebService地址
+     * @param xml 请求参数XML字符串
+     * @return 响应结果
+     */
+    public static String webserviceInvok(String endpoint, String xml) {
+
         log.info("webserviceInvok->xml-> {}", xml);
-		// 创建Service实例
-		Service service = new Service();
-		// 通过Service实例创建Call实例
-		Call call;
-		try {
-			call = (Call) service.createCall();
-			// 将WebService的服务路径加入到Call实例中，并为Call设置服务的位置
-			URL url = new URL(endpoint);
-			call.setTargetEndpointAddress(url);
-			// 调用WebService方法
-			call.setOperationName(new QName("http://SyncXmlServiceImpl.com/", "dataSync"));
-			// 调用WebService传入参数
-			call.addParameter("syncXml", org.apache.axis.encoding.XMLType.XSD_STRING,
-                    javax.xml.rpc.ParameterMode.IN);
-			call.setReturnType(org.apache.axis.encoding.XMLType.XSD_STRING);
-			String result = String.valueOf(call.invoke(new Object[] {xml}));
+        try {
+            // 创建ServiceClient实例
+            ServiceClient serviceClient = new ServiceClient();
+            
+            // 设置目标地址
+            Options options = new Options();
+            options.setTo(new EndpointReference(endpoint));
+            options.setAction("dataSync"); // 设置SOAPAction
+            serviceClient.setOptions(options);
+            
+            // 创建命名空间
+            OMFactory factory = OMAbstractFactory.getOMFactory();
+            OMNamespace namespace = factory.createOMNamespace("http://SyncXmlServiceImpl.com/", "syn");
+            
+            // 创建方法元素
+            OMElement methodElement = factory.createOMElement("dataSync", namespace);
+            
+            // 创建参数元素
+            OMElement paramElement = factory.createOMElement("syncXml", null); // 不设置命名空间
+            paramElement.setText(xml);
+            
+            // 添加参数到方法中
+            methodElement.addChild(paramElement);
+            
+            // 发送请求并获取响应
+            OMElement response = serviceClient.sendReceive(methodElement);
+
+            // 提取return元素的内容
+            OMElement returnElement = response.getFirstElement();
+            String result = returnElement.getText();
             log.info("webserviceInvok->result-> {}", result);
-			return  result;
-		} catch (Exception e) {
-			log.error("webserviceInvok->Exception->",e);
-			return "<?xml version=\"1.0\" encoding=\"GBK\"?><returnData><status>0</status><errcode></errcode><errormsg>推送报错 "+e+"</errormsg></returnData>";
-		}
+            return result;
+        } catch (Exception e) {
+            log.error("webserviceInvok->Exception->", e);
+            return "<?xml version=\"1.0\" encoding=\"GBK\"?><returnData><status>0</status><errcode></errcode><errormsg>推送报错 " + e + "</errormsg></returnData>";
+        }
+    }
+
+    	public static void main(String[] args) {
+		// webservice调用
+		String xml="<root><privateKey>UAP_2oSY90</privateKey><srcContent></srcContent><dataContent><syncContent dataType=\"1\" operType=\"1\"><syncUnicode>0410000043</syncUnicode><newContent><baseInfo><deptName>顺丰</deptName><deptUniCode>0410000043</deptUniCode><showNum>9999</showNum><deptStatus>1</deptStatus><isCorp>1</isCorp></baseInfo><parentInfo><parentCode>MRBM</parentCode></parentInfo></newContent></syncContent></dataContent></root>";
+		 xml="<root><privateKey>UAP_2oSY90</privateKey><srcContent></srcContent><dataContent><syncContent dataType=\"2\" operType=\"1\"><syncUnicode>zhangs</syncUnicode><newContent><baseInfo><loginName>zhangs</loginName><userName>张三</userName><secLevel>5</secLevel><accountStatus>1</accountStatus></baseInfo><parentInfo><parentCode>MRBM</parentCode></parentInfo></newContent></syncContent></dataContent></root>";
+		System.out.println(xml);
+		webserviceInvok("http://10.3.46.78:8090/securedoc/clientinterface/syncData/ISyncWebService",xml);
 	}
 }
